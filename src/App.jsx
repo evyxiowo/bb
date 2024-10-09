fe0468fa7efbd73bdb3155e0290a7309bd7a56d2
Xievy <xuhvm3@gmail.com>
2024-10-09 13:39:32 +0530

    done with functioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+import data from "./data";
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [resource , setResource] = useState(data)
+  const removeData = () => {
+    setResource([])
+  }
+  return (
+    <div>
+      <h2>Birthday Reminder - Starter</h2>
+      <h2>Buday Today {resource.length}</h2>
+        {resource.map((people) => {
+          return(
+            <div key={people.id}>
+            <h1>Name: {people.name}</h1>
+            <h1>Age: {people.age}</h1>
+            <img src={people.image} alt="error fetching img" />
+            </div>
+          )
+
+        }
+        )}
+        <button onClick={removeData}>Remove Data</button>
+     
+    </div>
+  );
 };
 export default App;
